3ffe292e9603786573062037b6addc9187582e52
PriyanshuKSharma <priyanshu17ks@gmail.com>
2025-09-08 12:37:00 +0530

    Restore full navbar functionality on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,9 +2,18 @@ import Header from "@/components/LearningPlatform/Header";
 import Hero from "@/components/LearningPlatform/Hero";
 
 const Home = () => {
+  const handleNavigate = (view: string) => {
+    if (view === 'signin') {
+      window.location.hash = '#/signin';
+    } else if (view !== 'home') {
+      // For other nav items, redirect to signin first
+      window.location.hash = '#/signin';
+    }
+  };
+
   return (
     <div className="min-h-screen bg-background">
-      <Header />
+      <Header onNavigate={handleNavigate} currentView="home" />
       <Hero />
     </div>
   );
